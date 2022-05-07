80f5eea11f2087a11c25d5572a20de46f45b66f6
ifritdiezel <ifritdiezel@gmail.com>
2022-05-07 18:41:36 +0500

    fixed repeating items FOR REAL

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on("messageCreate", async message => {
     let itemcategory = Math.floor((Math.random()*strings.items.length));
     let itemsincategory = strings.items[itemcategory].length;
     let firstitemkey = Math.floor( (Math.random()*itemsincategory));
-    let seconditemkey = (firstitemkey + Math.floor( Math.random()*itemsincategory) + 1) % itemsincategory;
+    let seconditemkey = (firstitemkey + Math.floor( Math.random()*itemsincategory)) % itemsincategory;
 
     content = [
       strings.genericopeners.random() + " " + strings.items[itemcategory][firstitemkey] + " " + strings.comparisons.random() + " "+ strings.items[itemcategory][seconditemkey] + " " + strings.conditions.random() + ". " + capitalize(strings.finishers.random()),
